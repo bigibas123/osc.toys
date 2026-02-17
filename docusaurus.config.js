84f0c9facef450d27bdc69612201de1cbae16f89
Bas Dingemans <bas@dingemans.cc>
2026-02-17 22:55:08 +0100

    Update versions of dependencies and github pipeline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,12 @@ const config = {
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "throw",
+      onBrokenMarkdownImages: "throw",
+    }
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
